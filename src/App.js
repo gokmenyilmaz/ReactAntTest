609e543b7e4aa6d4af8c8df33f976fa2945b6562
gokmen.yilmaz <gokmen1977@gmail.com>
2020-04-28 01:54:57 +0300

    ilk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state={
+      Personeller:[{ad:"musa",soyad:"aydın"}, {ad:"kamil",soyad:"uzun"}]
+    }
+  }
+
+  onEkle=()=>{
+    var liste=this.state.Personeller;
+
+    liste.push({ad:"cem",soyad:"ozer"});
+
+    this.setState({Personeller:liste});
+
+  }
+
+  render() {
+    return <div>
+
+      <button onClick={()=>this.onEkle()}>Ekle</button>
+
+      {
+         this.state.Personeller.map(c =>
+        <li>{c.ad}</li>
+      )}
+
+    </div>;
+  }
+ 
 }
 
-export default App;
+
