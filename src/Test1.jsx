cf1be0e13337a65fe1be573ea69467c7e5fd21b0
gokmen.yilmaz <gokmen1977@gmail.com>
2020-04-30 02:36:08 +0300

    uu

--- src/Test1.jsx
+++ src/Test1.jsx
@@ -1,35 +1,29 @@
 import React, { Component } from "react";
 import dialogService from "./DialogService";
-import KurumSec from "./KurumSec"
+import KurumSec from "./KurumSec";
 
 import { messageService } from "./message.service";
 
 export default class Test1 extends Component {
-
-  componentDidMount()
-  {
-    messageService.getMessage().subscribe(message => {
+  componentDidMount() {
+    messageService.getMessage().subscribe((message) => {
       if (message) {
         console.log(message);
-      }});
+      }
+    });
   }
-
-
 
   async Onayla() {
     var frm = dialogService.create();
-    
+
     var x = await frm.show({
       title: "Başlama/Erteleme Bildirimi Gereken İşler",
-      formComponent:(<KurumSec/>),
-      okText:"Tamam",
-      cancelText:"İptal"
-
+      formComponent: <KurumSec />,
+      okText: "Tamam",
+      cancelText: "İptal",
     });
 
     console.log(x);
-
-    
   }
 
   render() {
