7b74c51a7864811993747feb72416e9327fe40db
gokmen.yilmaz <gokmen1977@gmail.com>
2020-05-02 02:02:17 +0300

    uu

--- src/TestReactTable.jsx
+++ src/TestReactTable.jsx
@@ -1,11 +1,71 @@
-import React, { Component } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Table, Tag } from 'antd';
+const { Column, ColumnGroup } = Table;
 
-export default class TestReactTable extends Component {
-    render() {
-        return (
-            <div>
-                React table
-            </div>
-        )
-    }
+export default class TestReactTable extends React.Component {
+  data = [
+    {
+      key: "1",
+      firstName: "John",
+      lastName: "Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      firstName: "Jim",
+      lastName: "Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      firstName: "Joe",
+      lastName: "Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+
+
+  Ekle=()=>{
+      var x={
+        key: "3",
+        firstName: "Joe",
+        lastName: "Black",
+        age: 32,
+        address: "Sidney No. 1 Lake Park",
+        tags: ["cool", "teacher"],
+      };
+
+      this.data.push(x);
+
+  }
+
+  render() {
+
+
+
+    return (
+      <div>
+
+        <button onClick={()=>this.Ekle()}>Ekle</button>
+
+        <Table dataSource={this.data}>
+          <ColumnGroup title="Name">
+            <Column title="First Name" dataIndex="firstName" key="firstName" />
+            <Column title="Last Name" dataIndex="lastName" key="lastName" />
+          </ColumnGroup>
+          <Column title="Age" dataIndex="age" key="age" />
+          <Column title="Address" dataIndex="address" key="address" />
+        </Table>
+      </div>
+    );
+  }
 }
